a8b77f2bedc5256faf6b2523b60bcca507efe3e0
Tobias Görtz <31067358+tobias-goertz@users.noreply.github.com>
2021-12-02 19:08:07 +0100

    :recycle: decrease number of users
    decrease the number for faster seeding process

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
-99.times do |n|
+20.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -22,7 +22,7 @@ end
 
 # Generate microposts for a subset of users.
 users = User.order(:created_at).take(6)
-50.times do
+5.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
@@ -30,7 +30,7 @@ end
 # Create following relationships.
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..10]
+followers = users[3..15]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
