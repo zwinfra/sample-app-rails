b496a4e12459db12237c2d021c400ea9833cb323
Tobias Görtz <31067358+tobias-goertz@users.noreply.github.com>
2021-12-21 10:23:20 +0000

    :recycle: improve usability of sample app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         reset_session
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         log_in user
-        redirect_to forwarding_url || user
+        redirect_to forwarding_url || root_url
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
